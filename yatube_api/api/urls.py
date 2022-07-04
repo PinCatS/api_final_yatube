ff7d86c9c07909999f071b5f5e3bf84665712eda
Sergey Li <pincats@gmail.com>
2022-07-05 01:59:13 +0300

    Remove yasg code

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,5 @@
 from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
-from django.urls import include, path, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -19,34 +16,4 @@ router.register(
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls.jwt')),
-]
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Yatube API",
-        default_version='v1',
-        description="Документация для приложения posts проекта Yatube",
-        contact=openapi.Contact(email="admin@yatube.ru"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-urlpatterns += [
-    re_path(
-        r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0),
-        name='schema-json',
-    ),
-    re_path(
-        r'^swagger/$',
-        schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui',
-    ),
-    re_path(
-        r'^redoc/$',
-        schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc',
-    ),
 ]
